c51597639cebef2984f10f189ddc726da17931b8
adeeba reaz <adeebarivera@outlook.com>
2020-04-20 05:47:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
+# it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
+#       expect { countdown(10) }.to output(countdown_output).to_stdout
+#     end
 
-def countdown
+
+def countdown (int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -=1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    sleep(1)
+    i -= 1
+  end
+end
+
+
