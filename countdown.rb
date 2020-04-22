7cc1588282e9d1880cb41e00e169235fd51d92fd
adeeba reaz <adeebarivera@outlook.com>
2020-04-22 03:33:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
 #       expect { countdown(10) }.to output(countdown_output).to_stdout
 #     end
@@ -21,3 +22,38 @@ def countdown_with_sleep(i)
 end
 
 
+=======
+def countdown_with_sleep (int)
+  while int > 0
+    puts "#{int} SECOND(S)"
+    int -=1
+  end
+  "HAPPY NEW YEAR"
+end
+
+countdown_with_sleep (10)
+
+
+def countdown_with_sleep (x)
+  while x > 0
+    puts "#{x} SECOND(S)"
+    int -=1
+    sleep 1
+  end
+  "HAPPY NEW YEAR"
+end
+
+countdown_with_sleep (10)
+
+
+def countdown_with_sleep (i)
+  while y > 0
+    puts "#{i} SECOND(S)"
+    int -=1
+    sleep 1
+  end
+  "HAPPY NEW YEAR"
+end
+
+countdown_with_sleep (10)
+>>>>>>> eb886510ecb2dd5d4ba154e75591e313ce51c6c4
